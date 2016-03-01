ea6aaa19bf7276eaac81707b9386ac826a9fa376
Thomas Harman <tharman@expedia.com>
2016-02-29 19:53:21 -0800

    1. Always dance when app re-opened. 2. Toggle between greedy and last fortune shown. 3. some code refactoring to keep track of state. 4. Only start dancing() when app didAppear() - solves issue where we didn't see the fortune dance

--- FortuneCookie WatchKit Extension/FortuneInterfaceController.swift
+++ FortuneCookie WatchKit Extension/FortuneInterfaceController.swift
@@ -11,6 +11,14 @@ import Foundation
 
 class FortuneInterfaceController: WKInterfaceController {
     
+    enum FortuneState : Int {
+        case greedy
+        case showingFortune
+        case dancing
+    }
+    
+    let GREEDY_FORTUNE_IMAGE_NAME = "fortune-guy-greedy"
+    let SPEECH_BUBBLE_FORTUNE_IMAGE_NAME = "fortune-guy-speech-bubble"
     let YOU_ARE_GREEDY_STR = "Wow, you're greedy!\n Come back tomorrow for a New fortune"
     let RAN_OUT_OF_FORTUNES_HAPTIC = WKHapticType.Failure
     let NEW_FORTUNE_HAPTIC = WKHapticType.DirectionDown
@@ -23,39 +31,43 @@ class FortuneInterfaceController: WKInterfaceController {
     let fortuneLabelHeight = CGFloat(50)
     var isDancing = false
     var isFortuneQueuedToShow = false
+    var currentState: FortuneState? = nil
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
 //        nsDataFacade.reset()
     }
-
+    
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
+        // Called when watch interface is active and able to be updated.
+        // Can be called when interface is not visible.
         super.willActivate()
-        if (self.canShowFortune()) {
-            showDancingFortune()
-            showFortune(true)
-        }
-        else {
-            showLastFortune()
+        let appRunning = (self.currentState != nil)
+        if (appRunning) { // app already running on watch so didAppear() won't be called. Show fortune
+            showNewFortune(true)
         }
     }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    
+    override func didAppear() {
+        // NOT called when app already running on watch
+        super.didAppear()
+        showNewFortune(true)
     }
 
     @IBAction func fortuneCookieBtnTapped() {
-        showFortune(false)
+        showNewFortune(false)
     }
     
-    private func showFortune(isShowDelayed: Bool) {
+    private func showNewFortune(danceBefore: Bool) {
         if (isFortuneQueuedToShow) {
             return
         }
         
-        let delaySeconds = isShowDelayed ? 4.0 : 0.0
+        if (danceBefore) {
+            dance()
+        }
+        
+        let delaySeconds = danceBefore ? 4.0 : 0.0
         let delay = delaySeconds * Double(NSEC_PER_SEC)
         let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
         
@@ -64,29 +76,41 @@ class FortuneInterfaceController: WKInterfaceController {
             self.hideDancingFortune()
             
             print("can show fortune: \(self.canShowFortune())")
-            let canShowFortune = self.canShowFortune() // true
-            if (canShowFortune) {
+            let canShowNewFortune = self.canShowFortune() // true
+            if (canShowNewFortune) {
                 let fortune = self.fortunesModel.getFortune()
 //                let fortune = "Open me on your watch. iPhone app coming soon..."
-                let hapticType = (fortune == self.fortunesModel.RAN_OUT_OF_FORTUNES_STR) ? self.RAN_OUT_OF_FORTUNES_HAPTIC : self.NEW_FORTUNE_HAPTIC
-                self.nsDataFacade.saveFortuneLastAccessedNow() // TODO: perhaps this should be in the model. Not the view controller
-                self.playHaptic(hapticType)
-                self.fortuneLabel.setText(fortune)
-                self.fortuneCookieButton.setBackgroundImageNamed("fortune-guy-speech-bubble")
+                self.showFortune(fortune!)
+            }
+            else if (self.currentState == FortuneState.greedy || self.currentState == nil) {
+                self.showLastFortune()
             }
             else {
-                self.playHaptic(self.RAN_OUT_OF_FORTUNES_HAPTIC)
-                self.fortuneLabel.setText(self.YOU_ARE_GREEDY_STR)
-                self.fortuneCookieButton.setBackgroundImageNamed("fortune-guy-greedy")
+                self.showGreedyFortune()
             }
             
             self.isFortuneQueuedToShow = false
         })
     }
+    
+    private func showGreedyFortune() {
+        self.currentState = FortuneState.greedy
+        self.playHaptic(self.RAN_OUT_OF_FORTUNES_HAPTIC)
+        self.fortuneLabel.setText(self.YOU_ARE_GREEDY_STR)
+        self.fortuneCookieButton.setBackgroundImageNamed(self.GREEDY_FORTUNE_IMAGE_NAME)
+    }
+    
+    private func showFortune(fortune: String) {
+        self.currentState = FortuneState.showingFortune
+        let hapticType = (fortune == self.fortunesModel.RAN_OUT_OF_FORTUNES_STR) ? self.RAN_OUT_OF_FORTUNES_HAPTIC : self.NEW_FORTUNE_HAPTIC
+        self.nsDataFacade.saveFortuneLastAccessedNow() // TODO: perhaps this should be in the model. Not the view controller
+        self.playHaptic(hapticType)
+        self.fortuneLabel.setText(fortune)
+        self.fortuneCookieButton.setBackgroundImageNamed(self.SPEECH_BUBBLE_FORTUNE_IMAGE_NAME)
+    }
 
     private func showLastFortune() {
-        self.fortuneLabel.setText(self.fortunesModel.getLastShownFortune())
-        self.fortuneCookieButton.setBackgroundImageNamed("fortune-guy-speech-bubble")
+        self.showFortune(self.fortunesModel.getLastShownFortune())
     }
 
     private func hideDancingFortune() {
@@ -98,7 +122,7 @@ class FortuneInterfaceController: WKInterfaceController {
         self.isDancing = false
     }
     
-    private func showDancingFortune() {
+    private func dance() {
         if (self.isDancing) {
             return
         }
